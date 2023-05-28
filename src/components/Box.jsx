d8bd9b9ff24287f96dc4c708bc6dd4a6cafd110a
Yusha Tahlil <ymmttmy5@gmail.com>
2023-05-28 19:35:49 +0530

    Final

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -3,8 +3,13 @@ import React from "react";
 export const Box = ({ value, onClick, highlight }) => {
   const style = value == "X" ? "X" : "O";
   const highlightStyle = highlight ? "highlight" : "";
+  const cursorStyle = value ? "not-allowed" : "pointer";
   return (
-    <button className={`box ${style} ${highlightStyle}`} onClick={onClick}>
+    <button
+      className={`box ${style} ${highlightStyle}`}
+      onClick={onClick}
+      style={{ cursor: cursorStyle }}
+    >
       {value}
     </button>
   );
